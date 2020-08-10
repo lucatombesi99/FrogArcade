be2eaa8c2a99e91a933f093bbee17c2e2ba6e7b7
Luca Tombesi <lucatombesi99@gmail.com>
2020-08-10 18:00:27 +0200

    changed 10/08/2020

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,32 +1,47 @@
 package sample;
 
 import gameSystem.gameScene;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 
 
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 //ricorda di dare gli ID alle componenti
 
 public class Controller {
-
-
-
-    @FXML
-    private ToggleGroup difficulty;
-
-
     private static boolean autoPlay=true;
 
+    ObservableList<String> difficultyList= FXCollections.observableArrayList("easy","medium","hard");
+
     @FXML
-    private void volumeClicked(ActionEvent event){
+    private ChoiceBox<String> difficultyBox;
+
+    public static boolean isAutoPlay() {
+        return autoPlay;
+    }
+
+   @FXML
+   private void initialize() {
+       difficultyBox.setValue("easy");
+       difficultyBox.setItems(difficultyList);
+
+   }
+
+
+    public void exitButtonClicked(ActionEvent event){
+
+    }
+    public void audioButtonClicked(ActionEvent event) throws IOException{
         if(autoPlay) {
             autoPlay=false;
             menuActions.pause();
@@ -36,26 +51,23 @@ public class Controller {
         }
 
     }
-
-    @FXML
-    private void showScore(ActionEvent event){
-
+    public void scoreButtonClicked(ActionEvent event){
         menuActions.scoreRecord();
-    }
 
+    }
 
 
     public void newGameButtonClicked(ActionEvent event) throws IOException {
 
         menuActions.stop();
         gameScene g= new gameScene();
-        RadioMenuItem selectedRadioButton;
-        selectedRadioButton = (RadioMenuItem) difficulty.getSelectedToggle();
-        String toggleGroupValue = selectedRadioButton.getText();
+      String choiceBoxValue= difficultyBox.getValue().toString();
+   //selectedRadioButton = (RadioMenuItem) difficulty.getSelectedToggle();
+
         int difficulty;
-        if(toggleGroupValue.equals("Easy"))
+        if(choiceBoxValue.equals("easy"))
             difficulty=0;
-        else if(toggleGroupValue.equals("Medium"))
+        else if(choiceBoxValue.equals("medium"))
             difficulty=1;
         else
             difficulty=2;
@@ -68,3 +80,8 @@ public class Controller {
 
 
 }
+/*
+
+ onAction="#newGameButtonClicked"
+
+ */
