05627caee272c04350acf677470b5a0363a846aa
Luca Tombesi <lucatombesi99@gmail.com>
2020-08-11 15:57:25 +0200

    ho ripreso la tua parte e la ho unita alla mia

--- src/gameSystem/gameScene.java
+++ src/gameSystem/gameScene.java
@@ -1,57 +1,86 @@
 package gameSystem;
 
-
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import java.io.File;
 
 
 public class gameScene {
+
+    final static String AUDIO_PATH = "Resources\\Audio\\";
+    final static String IMAGES_PATH = "Resources\\Images\\";
+
     Media media;
     Button pauseButton;
+    Label timeLabel;
+    Label difficultyLabel;
+    AnchorPane backgroundScene;
+    ImageView backgroundImage;
+    String level = "";
 
-public  void startGame(Stage primaryStage,int difficulty){
-    String path ="src\\gameSystem\\theme.mp3";
-    media=new Media(new File(path).toURI().toString());
-    pauseButton=new Button("Pause");
+    public  void startGame(Stage primaryStage, int difficulty){
 
-    MediaPlayer mediaPlayer=new MediaPlayer(media);
-    mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-    mediaPlayer.play();
+        //MUSICA di SOTTOFONDO
+        media = new Media(new File(AUDIO_PATH + "theme.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
 
-    BorderPane layout = new BorderPane();
+        //Label level
+        if (difficulty == 0)
+            level += "EASY";
+        else if(difficulty ==1)
+            level += "MEDIUM";
+        else
+            level += "HARD";
 
-    ImageView img = new ImageView();
-    img.setImage(new Image(getClass().getResource("media\\iKogsKW.png").toExternalForm(),600, 800, true, true));
+        //SCRITTA IN ALTO
+        pauseButton = new Button("||");
+        timeLabel = new Label("Time:");
+        timeLabel.setFont(new Font("Calibri", 20));
+        difficultyLabel= new Label("Difficulty:" + level);
+        difficultyLabel.setFont(new Font("Calibri", 20));
 
-    layout.getChildren().addAll(img);
-    layout.setTop(pauseButton);
-    layout.getStylesheets().add(getClass().getResource("Back.css").toExternalForm());
+        backgroundScene = new AnchorPane();
+        backgroundScene.maxHeight(400);
+        backgroundScene.maxWidth(750);
+        backgroundScene.setPrefSize(400,750);
 
+        //pos Bottone Pausa
+        AnchorPane.setTopAnchor(pauseButton,10.0);
+        AnchorPane.setLeftAnchor(pauseButton,20.0);
 
-    Scene scene = new Scene(layout,500,500);
+        //pos Etichetta Time
+        AnchorPane.setTopAnchor(timeLabel,10.0);
+        AnchorPane.setLeftAnchor(timeLabel,100.0);
 
-
-    primaryStage.setScene(scene);
-
-    pauseButton.setOnAction(e->{
-        pauseClass.pause(mediaPlayer);
-
-
-    });
-
-
-}
-
-
-
-
+        //pos Etiichetta Difficoltà
+        AnchorPane.setTopAnchor(difficultyLabel,10.0);
+        AnchorPane.setLeftAnchor(difficultyLabel,200.0);
 
 
+        Image backgroundImageURL= new Image(new File(IMAGES_PATH + "iKogsKW.png").toURI().toString());
+        backgroundImage = new ImageView(backgroundImageURL);
+
+        //pos Image
+        AnchorPane.setTopAnchor(backgroundImage, 40.0);
+
+        backgroundScene.getChildren().addAll(pauseButton,timeLabel,difficultyLabel, backgroundImage);
+
+
+        primaryStage.setScene(new Scene(backgroundScene, 384,580));
+
+
+        pauseButton.setOnAction(e->{
+            pauseClass.pause(mediaPlayer);
+        });
+
+    }
 }
